92e290158083682cf799990253f4db8deb815c13
Benedict Su <benedict.suratanakavikul@zenjob.com>
2023-06-26 17:46:32 +0200

    init spring project

---
+++ src/main/java/com/brewery/microservicebreweryclient/MicroserviceBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.brewery.microservicebreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBreweryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceBreweryClientApplication.class, args);
+    }
+
+}
